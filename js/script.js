30750d15bb4b2516ff7d782b8cdfdd16f2af7d4c
natividadesusana <susanajdsn@gmail.com>
2023-01-07 02:13:01 -0300

    activating color in finalize order button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function selectedFirst(selectedOption) {
-    const select = document.querySelector('.teste1 .selecting');
+    const select = document.querySelector('.foods .selecting');
     if (select !== null) {
         select.classList.remove('selecting');
     }
@@ -7,7 +7,7 @@ function selectedFirst(selectedOption) {
 }
 
 function selectedSecond(selectedOption) {
-    const select = document.querySelector('.teste2 .selecting');
+    const select = document.querySelector('.drinks .selecting');
     if (select !== null) {
         select.classList.remove('selecting');
     }
@@ -15,9 +15,24 @@ function selectedSecond(selectedOption) {
 }
 
 function selectedThird(selectedOption) {
-    const select = document.querySelector('.teste3 .selecting');
+    const select = document.querySelector('.desserts .selecting');
     if (select !== null) {
         select.classList.remove('selecting');
     }
     selectedOption.classList.toggle('selecting');
 }
+
+function finalize(finishing) {
+
+    const button = document.querySelector('.finalize-order');
+    if (button !== null) {
+        button.classList.remove('selected-button');
+    }
+    finishing.classList.add('selected-button');
+}
+
+// function verificaOpcoesSelecionadas() {
+//     if (teste1 !== null) {
+//         console.log("jogo foi selecionado")
+//     }
+// }
